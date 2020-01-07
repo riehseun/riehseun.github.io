14394722e51d7c0a960695a08e891ca40d458b34
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-01-06 21:00:04 -0500

    update

--- algorithm/string/print-all-palindromic-partitions.py
+++ algorithm/string/print-all-palindromic-partitions.py
@@ -26,5 +26,56 @@ b-ana-n-a
 b-anana
 """
 
-def get_parlindrom_partitions():
-	return ""
+def get_parlindrom_partitions(string):
+	# breaking the input string into individual chars make it parlindome partition
+	# for all partitioned sets of input string must be parlindrom
+	# parlindrom is when either sub-string is a single char or symmetric with odd # of chars
+
+	# placeholder for the result
+	parlindromic_partitions = []
+
+	# possible chunks of string division are 2,3,4,...,len(string). According number of index where division should take places are 1,2,3,...len(String)-1
+	# for each number of index, find all possible partitions
+
+	# for each array containing partitiioned sets of strings, add to result if is_everything_parlindrom() returns True
+	for partition in partitions:
+		if(is_everything_parlindrom(partition)):
+			parlindromic_partitions.append(partition)
+
+
+def is_everything_parlindrom(string_array):
+	""" takes an array of strings and return true if all elements in the array are parlindroms """
+	for string in string_array:
+		if (is_parlindrom(string) == False):
+			return False
+	return True
+
+def is_parlindrom(string):
+	""" takes a string and return true if it is parlindrom """
+
+	# single char is parlindrom
+	if (len(string) == 1):
+		return True
+
+	# even number is not parlindrom
+	if (len(string) % 2 == 0):
+		return False
+
+	# only need to check half of index minus the middle char
+	string_index_to_check = int(len(string)/2-0.5)
+
+	# if asymmetry is found, return False
+	for i in range(string_index_to_check):
+		if (string[i] != string[len(string)-1-i]):
+			return False
+
+	# passing above test means string is symmetric
+	return True
+
+assert (is_parlindrom("naban") == True)
+assert (is_parlindrom("n") == True)
+assert (is_parlindrom("nabanan") == False)
+assert (is_parlindrom("nanana") == False)
+
+assert (is_everything_parlindrom(["naban", "a"]) == True)
+assert (is_everything_parlindrom(["naban", "nabanan"]) == False)
