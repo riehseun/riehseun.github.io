d3b305f94695d10d6b3570a3308be8508a35da55
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-02-11 21:04:27 -0500

    update

--- algorithm/string/print_all_palindromic_partitions.py
+++ algorithm/string/print_all_palindromic_partitions.py
@@ -27,21 +27,25 @@ b-anana
 
 Usage:
 
-    python3 words.py &lt;URL&gt;
+    python print-all-parlindrom-partitions.py
 
 """
 
 import math
+
 def get_parlindrom_partitions(string):
-	# breaking the input string into individual chars make it parlindome partition
-	# for all partitioned sets of input string must be parlindrom
-	# parlindrom is when either sub-string is a single char or symmetric with odd # of chars
+	"""	Produces a list of parlindrom partitions of input string
 
-	# placeholder for the result
-	parlindromic_partitions = []
+	Args:
+		string: string to be partitioned into substrings
 
-	# Generate 2^(len(string)-1)) binaries where 0 = don't partition and 1 = partition. Store them into an array. Then split strings based on 1s'
-	# Create an array of size 2^len(strong)-1 of empty strings. For half of element, add 0. For the other half, add 1. Use recursion to apply this rule
+	Returns:
+		A list containing substring of the input string that are all parlindromes
+
+	"""
+
+	parlindromic_partitions = [] # list to store the result
+
 	binary_array = []
 	for i in range(0, int(math.pow(2, len(string)-1))):
 		binary_array.append("")
@@ -56,65 +60,81 @@ def get_parlindrom_partitions(string):
 		string_partition = []
 		for index, value in enumerate(binary):
 			if (value == "1"):
-				# print(string[start_index_to_split:index])
 				string_partition.append(string[start_index_to_split:int(index+1)])
 				start_index_to_split = int(index+1)
-		# append left over substring after the last split
-		string_partition.append(string[start_index_to_split:len(string)])
+		string_partition.append(string[start_index_to_split:len(string)]) # append left-over substring after the last split
 		count = count + 1
-		print(string_partition)
 		if (is_everything_parlindrom(string_partition)):
 			parlindromic_partitions.append(string_partition)
 
-	print(count)
 	return parlindromic_partitions
 
 def generate_binary_string(array, start_index, number):
-	""" takes array of empty string, start_index, number of digits to generate"""
+	""" populates input list with binaries where 0 represents - don't partition and 1 represents - partition
+
+	Args:
+		array: list to be populated with binaries
+		start_index: index of array to start populating binaries from
+		number: indicates that (2^number) of binaries should be generated
+
+	Returns:
+		list containing 2^(len(string)-1)) number of binaries
+	"""
+
 	if (number <= 0):
 		return array
 
 	number_of_binaries_to_generate = math.pow(2, number)
 	number_of_binaries_to_generate = int(number_of_binaries_to_generate)
 
-	for i in range(start_index, start_index + number_of_binaries_to_generate):
+	for i in range(start_index, start_index + number_of_binaries_to_generate): # append 0 to the first half and 1 to the other half
 		if (i<start_index + int(number_of_binaries_to_generate/2)):
 			array[i] += "0"
 		else:
 			array[i] += "1"
 
-	generate_binary_string(array, start_index, number-1) # generate for first half
-	start_index =  start_index + int(number_of_binaries_to_generate/2)
-	generate_binary_string(array, start_index, number-1) # generate for second half
+	generate_binary_string(array, start_index, number-1) # recurse appending 0's and 1's for first half (sub-list)
+	start_index =  start_index + int(number_of_binaries_to_generate/2) # updates start_index so that it starts from the beginning of second half
+	generate_binary_string(array, start_index, number-1) # recurse appending 0's and 1's for second half (sub-list)
 
 def is_everything_parlindrom(string_array):
-	""" takes an array of strings and return true if all elements in the array are parlindroms """
+	""" check if every element in the input list is parlindrom
+
+	Args:
+		string_array: list of strings
+
+	Returns:
+		bool indicating whether all string in the list is parlindrom or not
+	"""
+
 	for string in string_array:
 		if (is_parlindrom(string) == False):
 			return False
 	return True
 
 def is_parlindrom(string):
-	""" takes a string and return true if it is parlindrom """
+	""" check if string is parlindrom
 
-	# single char is parlindrom
-	if (len(string) == 1):
+	Args:
+		string: input string to be checked if it is parlindrom
+
+	Returns:
+		bool indicating whether input string is parlindrom or not
+	"""
+
+	if (len(string) == 1): # single char is parlindrom
 		return True
 
-	# even number is not parlindrom
-	if (len(string) % 2 == 0):
+	if (len(string) % 2 == 0): # even number is not parlindrom
 		return False
 
-	# only need to check half of index minus the middle char
-	string_index_to_check = int(len(string)/2-0.5)
+	string_index_to_check = int(len(string)/2-0.5) # only need to check half of index minus the middle char
 
-	# if asymmetry is found, return False
-	for i in range(string_index_to_check):
+	for i in range(string_index_to_check): # if asymmetry is found, return False
 		if (string[i] != string[len(string)-1-i]):
 			return False
 
-	# passing above test means string is symmetric
-	return True
+	return True # passing above tests mean that string is symmetric, thus parlindrom
 
 print (get_parlindrom_partitions("IDeserve"))
 print (get_parlindrom_partitions("banana"))
