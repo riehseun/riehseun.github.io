c2846f2780c460fee9661c6cc742203b3d23cf37
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-02-19 20:23:42 -0500

    update

--- algorithm/string/word_break_problem.py
+++ algorithm/string/word_break_problem.py
@@ -21,16 +21,56 @@ def is_string_breakable(string, word_dictionary):
 
 	Args:
 		string : a string
-		word_dictionary : a list containing words
+		word_dictionary : a list containing words that are strings
 
 	Returns:
 		a bool indicating whether input string is breakable or not
 
 	"""
+	substrings = get_sub_strings(string, word_dictionary)
+	print(substrings)
+	if (len(substrings) == 0):
+		return False
+	for word in substrings:
+		"""break input string by word. apply get_sub_strings() on those two parts. recurse until substrings list is empty
+		if broken part equals one of words, case True
+		if substring list is empty, case False
+		"""
+		front_part = string.split(word)[0]
+		back_part = string.split(word)[1]
+		if (front_part not in word_dictionary):
+			is_string_breakable(front_part, word_dictionary)
+
+		if (back_part not in word_dictionary):
+			is_string_breakable(back_part, word_dictionary)
+	print("case True")
+	return True
+
+
+def get_sub_strings(string, word_dictionary):
+	"""
+
+	Args:
+		string : a string
+		word_dictionary : a list containing words that are strings
+
+	Returns:
+		a list of strings from word_dictionary that are sub-strings of input string
+
+
+	"""
+	substrings = []
+
+	for word in word_dictionary:
+		if word in string:
+			substrings.append(word)
+	return substrings
 
 
 word_dictionary = ["arrays", "dynamic", "heaps", "IDeserve", "learn", "learning", "linked", "list", "platform", "programming", "stacks", "trees"]
 assert(is_string_breakable("IDeservelearningplatform", word_dictionary) == True)
+assert(is_string_breakable("dynamic", word_dictionary) == True)
 assert(is_string_breakable("arraysprogramminglist", word_dictionary) == True)
 assert(is_string_breakable("listprogrammingheap", word_dictionary) == False)
+assert(is_string_breakable("love", word_dictionary) == False)
 
