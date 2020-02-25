6dcd245ef8e7a448fb4906d9c9dbcfbf1fd30ee1
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-02-24 19:21:47 -0500

    update

--- algorithm/string/minimum-trials-to-reach-from-source-to-destination-word.py
+++ algorithm/string/minimum-trials-to-reach-from-source-to-destination-word.py
@@ -20,7 +20,7 @@ Usage:
 
 """
 
-def run(source_word, destination_word, dictionary):
+def run(count, source_word, destination_word, dictionary):
 	"""
 		Args:
 			source_word:
@@ -31,19 +31,49 @@ def run(source_word, destination_word, dictionary):
 			interger represeting # of trials (-1 representing impossible scenario)
 
 	"""
-	count = 0
 
-	if (len(source_word) > len(destination_word)):
-		new_word = source_word[1:]
+	print(source_word +" vs "+ destination_word)
+	if (source_word == destination_word):
+		return count
+
+	if (len(source_word) > len(destination_word)): # delete char
+		left_over = source_word.replace(destination_word, "") # find out mismatches
+		new_word = source_word.replace(left_over[0], "") # pick one mismatch and remove it
+
 		if new_word not in dictionary:
 			return -1
-		else
-			count += 1
+		else:
+			new_count = count + 1
+			return run(new_count, new_word, destination_word, dictionary)
 
-	elif (len(source_word) < len(destination)):
-		for i in range(0, len(destination_word)-len(source_word)):
-			insert(source_word[i])
-	else:
+	elif (len(source_word) < len(destination_word)): # insert char
+		missing = desination_word.replace(source_word, "")
+		new_word1 = missing[0] + source_word # try adding char at front
+		new_word2 = source_word + missing[0] # try adding char at back
+		new_word = ""
+		if (len(desination_word.replace(new_word1, "")) < len(missing)): # if adding char at front is correct, length of string that is mismatching must decrease
+			new_word = new_word1
+		else:
+			new_word = new_word2
+
+		if new_word not in dictionary:
+			return -1
+		else:
+			new_count = count + 1
+			return run(new_count, new_word, destination_word, dictionary)
+
+	else: # update char
+		for i in range(0, len(source_word)):
+
+			if (source_word[i] != destination_word[i]):
+				new_word = source_word[0:i] + destination_word[i] + source_word[i+1:len(source_word)]
+				if new_word not in dictionary:
+					return -1
+				else:
+					new_count = count + 1
+					return run(new_count, new_word, destination_word, dictionary)
 
 
-def delete(string, index):
+assert(run(0, "AICC", "ICC", ["BCCI","AICC","ICC","CCI","MCC","MCA", "ACC"]) == 1)
+assert(run(0, "AICC", "MCA", ["BCCI","AICC","ICC","CCI","MCC","MCA", "ACC"]) == 3)
+assert(run(0, "AICC", "BCCI", ["BCCI","AICC","ICC","CCI","MCC","MCA", "ACC"]) == -1)
