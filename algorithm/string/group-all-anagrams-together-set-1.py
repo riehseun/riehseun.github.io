19314068079cd00cfa8077f55329202ddbabf5b6
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-02-26 18:38:03 -0500

    update

---
+++ algorithm/string/group-all-anagrams-together-set-1.py
@@ -0,0 +1,30 @@
+"""
+Given an array of strings, sort the array in such a way that all anagrams are grouped together.
+
+For example, if the given array input is {"abcd","abc","abce", "acd","abdc"} then output array should be {"abc","abcd","abdc","abce","acd"}.
+
+Note that order among non-anagram strings is not important here.
+
+Instead of returning above array as output, your program could have returned an array having different order of strings
+but strings "abcd" and "abdc" must be placed next to each other with no specific ordering requirement among them.
+
+So the output array - {"abdc","abcd","abc","abce","acd"} would also be a correct output.
+
+Usage:
+
+	group-all-anagrams-together-set-1.py
+
+"""
+
+
+def run():
+	"""
+
+	Args:
+
+
+	Returns:
+
+
+
+	"""
