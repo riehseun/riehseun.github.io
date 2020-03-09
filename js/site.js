e492f162d82fc8ecd8bdb4937e5cb944a19dc8ae
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-03-08 21:03:01 -0400

    update

--- js/site.js
+++ js/site.js
@@ -1,7 +1,39 @@
 /**
-	JS
+This applies coding sytle for different programming languages for <pre><code>
 */
+
 hljs.initHighlightingOnLoad();
+
+
+/**
+This makes anchor link go some pixels above where it's linked to
+*/
+
+// The function actually applying the offset
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+  }
+}
+
+// Captures click events of all <a> elements with href starting with #
+$(document).on('click', 'a[href^="#"]', function(event) {
+  // Click events are captured before hashchanges. Timeout
+  // causes offsetAnchor to be called after the page jump.
+  window.setTimeout(function() {
+    offsetAnchor();
+  }, 0);
+});
+
+// Set the offset when entering page with hash present in the url
+window.setTimeout(offsetAnchor, 0);
+
+
+
+
+
+
+
 
 // var elements = []
 // elements.push("devops")
