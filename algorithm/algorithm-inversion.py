2c1636184f607e31e42f994044f8c86f5eb31447
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-03-09 20:06:12 -0400

    update

--- algorithm/algorithm-inversion.py
+++ algorithm/algorithm-inversion.py
@@ -18,4 +18,60 @@ So if your answer is 1198233847, then just type 1198233847 in the space provided
 [TIP: before submitting, first test the correctness of your program on some small test files or your own devising. Then post your best test cases to the discussion forums to help your fellow students!]
 """
 
-def run()
+def run(integer_array, filepath):
+	"""
+	Computes # of inversion
+
+	Args:
+	file -- string representing location of files containing lots of number
+
+	Retunrs:
+	Integer representing the number of inversion
+	"""
+	if filepath != "":
+		with open(filepath, 'r') as line:
+			integer_array = line.read().split("\n")
+
+	# print(len(integer_array))
+
+	# base case (only two element in each array)
+
+	if len(integer_array) == 1:
+		return integer_array
+
+	if len(integer_array) == 2:
+		if int(integer_array[0]) > int(integer_array[1]):
+			temp = integer_array[0]
+			integer_array[0] = integer_array[1]
+			integer_array[1] = temp
+		return integer_array
+
+
+	first_half = integer_array[:int(len(integer_array)/2)]
+	second_half = integer_array[int(len(integer_array)/2):len(integer_array)]
+
+	sorted_first_half = run(first_half, "")
+	sorted_second_half = run(second_half, "")
+
+	print(sorted_first_half)
+	print(sorted_second_half)
+	# integer_array = sorted_first_half + sorted_second_half
+	sorted_integer_array = []
+	i = 0
+	j = 0
+	while i < len(first_half) and j < len(second_half):
+		if int(first_half[i]) < int(second_half[j]):
+			sorted_integer_array.append(first_half[i])
+			i += 1
+		else:
+			sorted_integer_array.append(second_half[j])
+			j += 1
+
+	integer_array = sorted_integer_array
+	return integer_array
+
+
+
+print(run([], "simple.txt"))
+# print(run([], "test.txt"))
+# print(run([], "algorithm-inversion.txt"))
