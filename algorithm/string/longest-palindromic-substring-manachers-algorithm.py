4bf74414eb0d1f7b8e7c8fd02971d7cd55e3c436
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-03-28 21:58:45 -0400

    update

--- algorithm/string/longest-palindromic-substring-manachers-algorithm.py
+++
@@ -1,7 +0,0 @@
-"""
-For string S, find the longest palindromic substring.
-Example: S = "bananas" LPS = "anana"
-
-Usage:
-	python longest-palindromic-substring-manachers-algorithm.py
-"""
--- algorithm/string/longest-palindromic-substring.py
+++
@@ -1,7 +0,0 @@
-"""
-Given a string S, find the longest palindromic substring
-
-Usage:
-	python longest-palindromic-substring.py
-"""
-
