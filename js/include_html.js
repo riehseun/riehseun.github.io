9be191f2b6aae3c36101309bb78d3cdd7b03ed3d
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2022-03-23 18:54:27 -0400

    update

--- js/include_html.js
+++ js/include_html.js
@@ -1,43 +1,13 @@
-// document.addEventListener("DOMContentLoaded", function(){
-//   let includes = document.getElementsByTagName('include');
-//   for(var i=0; i<includes.length; i++){
-//     let include = includes[i];
-//     load_file(includes[i].attributes.src.value, function(text){
-//         include.insertAdjacentHTML('afterend', text);
-//         include.remove();
-//     });
-//   }
-//   function load_file(filename, callback) {
-//     fetch(filename).then(response => response.text()).then(text => callback(text));
-//   }
-// });
-
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
-    }
+document.addEventListener("DOMContentLoaded", function(){
+  let includes = document.getElementsByTagName('include');
+  for(var i=0; i<includes.length; i++){
+    let include = includes[i];
+    load_file(includes[i].attributes.src.value, function(text){
+        include.insertAdjacentHTML('afterend', text);
+        include.remove();
+    });
   }
-}
-
-includeHTML();
+  function load_file(filename, callback) {
+    fetch(filename).then(response => response.text()).then(text => callback(text));
+  }
+});
