e64266e0ea3c4a49700ac0b2e589bace0094e23e
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2023-03-24 15:02:27 -0400

    update

--- leet/leet62.py
+++ leet/leet62.py
@@ -1,7 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-
         # Each cell container the number of unique path leading
         # up to it. The value at a cell is the addition of its left
         # and up cell.
@@ -17,7 +16,6 @@ class Solution:
             for _ in range(n+1):
                 dp[i].append(0)
 
-        
         for i in range(1,m+1):
             for j in range(1,n+1):
                 if i == 1 and j == 1:
